6c10d7a9b47cc0206556e5986a02e02687e5ed0b
Alvaro Lamadrid <aglamadrid19@gmail.com>
2020-10-24 05:22:12 -0400

    test apple pay

--- components/checkout/CheckoutForm.js
+++ components/checkout/CheckoutForm.js
@@ -13,7 +13,7 @@ const CheckoutForm = () => {
         currency: 'usd',
         total: {
           label: 'Demo total',
-          amount: 1099,
+          amount: 01,
         },
         requestPayerName: true,
         requestPayerEmail: true,
@@ -25,7 +25,7 @@ const CheckoutForm = () => {
       });
     }
   }, [stripe]);
-  
+
   if (paymentRequest) {
     return <PaymentRequestButtonElement options={{paymentRequest}} />
   }
@@ -33,5 +33,39 @@ const CheckoutForm = () => {
   // Use a traditional checkout form.
   return 'Insert your form or button component here.';
 }
+
+    paymentRequest.on('paymentmethod', async (ev) => {
+    // Confirm the PaymentIntent without handling potential next actions (yet).
+    const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(
+      clientSecret,
+      {payment_method: ev.paymentMethod.id},
+      {handleActions: false}
+    );
+  
+    if (confirmError) {
+      // Report to the browser that the payment failed, prompting it to
+      // re-show the payment interface, or show an error message and close
+      // the payment interface.
+      ev.complete('fail');
+    } else {
+      // Report to the browser that the confirmation was successful, prompting
+      // it to close the browser payment method collection interface.
+      ev.complete('success');
+      // Check if the PaymentIntent requires any actions and if so let Stripe.js
+      // handle the flow. If using an API version older than "2019-02-11" instead
+      // instead check for: `paymentIntent.status === "requires_source_action"`.
+      if (paymentIntent.status === "requires_action") {
+        // Let Stripe.js handle the rest of the payment flow.
+        const {error} = await stripe.confirmCardPayment(clientSecret);
+        if (error) {
+          // The payment failed -- ask your customer for a new payment method.
+        } else {
+          // The payment has succeeded.
+        }
+      } else {
+        // The payment has succeeded.
+      }
+    }
+  });
 
 export default connect(state => state)(CheckoutForm);
