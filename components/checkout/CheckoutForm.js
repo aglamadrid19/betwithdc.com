aa32d50a0600c7c2beb26b690a4df68c043df3c8
Alvaro Lamadrid <aglamadrid19@gmail.com>
2020-10-24 13:31:11 -0400

    fix deployed

--- components/checkout/CheckoutForm.js
+++ components/checkout/CheckoutForm.js
@@ -1,47 +1,49 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import {PaymentRequestButtonElement, useStripe} from '@stripe/react-stripe-js';
 import { connect } from 'react-redux';
 
 const CheckoutForm = () => {
-  const stripe = useStripe();
-  const [paymentRequest, setPaymentRequest] = useState(null);
+    const stripe = useStripe();
+    const [paymentRequest, setPaymentRequest] = useState(null);
 
-  useEffect(() => {
-    if (stripe) {
-      const pr = stripe.paymentRequest({
-        country: 'US',
-        currency: 'usd',
-        total: {
-          label: 'Demo total',
-          amount: 10,
-        },
-        requestPayerName: true,
-        requestPayerEmail: true,
-      });
-      pr.canMakePayment().then(result => {
-        if (result) {
-          setPaymentRequest(pr);
+    useEffect(() => {
+        if (stripe) {
+        const pr = stripe.paymentRequest({
+            country: 'US',
+            currency: 'usd',
+            total: {
+            label: 'Demo total',
+            amount: 10,
+            },
+            requestPayerName: true,
+            requestPayerEmail: true,
+        });
+        pr.canMakePayment().then(result => {
+            if (result) {
+            setPaymentRequest(pr);
+            }
+        });
         }
-      });
-    }
-  }, [stripe]);
+    }, [stripe]);
 
-  if (paymentRequest) {
-    return <PaymentRequestButtonElement options={{paymentRequest}} />
-  }
+    if (paymentRequest) {
+        return <PaymentRequestButtonElement options={{paymentRequest}} />
+    }
 
   // Use a traditional checkout form.
-  return (
-    <button onClick={this.handleAddToCart}
-        className="h-56 bg-black font-color-white pl-4 pr-4 d-flex align-items-center justify-content-center flex-grow-1" 
-        type="button" 
-        style={{width: `100%`}}
-    >
-        <Elements stripe={stripePromise}>
-            <CheckoutForm />
-        </Elements>
-    </button>  
-  )
-}
+  /**
+  * Render collections based on categories available in data
+  */
+    
+    return (
+            <button
+                className="h-56 bg-black font-color-white pl-4 pr-4 d-flex align-items-center justify-content-center flex-grow-1" 
+                type="button" 
+                style={{width: `100%`}}
+            >
+                Buy Now
+            </button>  
+        )
+    }
 
 export default connect(state => state)(CheckoutForm);
