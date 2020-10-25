21ec1f335d0aaf4b4cfeb6b6be30ec55856e3454
Alvaro Lamadrid <aglamadrid19@gmail.com>
2020-10-25 00:20:46 -0400

    idk

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 exports.handler = async event => {
-  console.log(process.env.STRIPE_SECRET)
+  
   return {
     statusCode: 200,
-    body: `Hello Alvaro Lamadrid`
+    body: `${console.log(process.env.STRIPE_SECRET)}`
+    
   }
 }
